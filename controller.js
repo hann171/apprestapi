ba59a44f4c2d0c8b7e494bb75ab7d40a3db511f3
Hengky Wijaya <hannw171@outlook.com>
2020-04-02 16:40:11 +0700

    berhasil menambahkan data mahasiswa

--- controller.js
+++ controller.js
@@ -3,30 +3,47 @@
 var response = require('./rest');
 var connection = require('./koneksi');
 
-exports.index = function(req,rest){
-    response.ok("Aplikasi rest api berjalan",rest)
+exports.index = function (req, rest) {
+    response.ok("Aplikasi rest api berjalan", rest)
 };
 
 //menampilkan semua data mahasiswa
-exports.tampilsemuamhs = function(req,rest){
-    connection.query('SELECT * from mahasiswa', function(error,rows,fields){
-        if(error){
+exports.tampilsemuamhs = function (req, rest) {
+    connection.query('SELECT * from mahasiswa', function (error, rows, fields) {
+        if (error) {
             connection.log(error);
-        }else{
+        } else {
             response.ok(rows, rest)
         }
     });
 };
 
 //menampilkan semua data mahasiswa sesuai id
-exports.tampilsemuamhsid = function(req,rest){
+exports.tampilsemuamhsid = function (req, rest) {
     let id = req.params.id;
-    connection.query('SELECT * from mahasiswa WHERE id_mahasiswa = ?',[id],
-    function(error,rows,fields){
-        if(error){
-            connection.log(error);
-        }else{
-            response.ok(rows,rest)
-        }
-    });
+    connection.query('SELECT * from mahasiswa WHERE id_mahasiswa = ?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                connection.log(error);
+            } else {
+                response.ok(rows, rest)
+            }
+        });
+};
+
+//menambahkan data mahasiswa
+exports.tambahmhs = function (req, rest) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES (?,?,?)',
+        [nim, nama, jurusan],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menambahkan data", rest)
+            }
+        });
 };
--- routes.js
+++ routes.js
@@ -11,4 +11,7 @@ module.exports = function(app){
 
     app.route('/tampil/:id')
         .get(jsonku.tampilsemuamhsid);
+    
+    app.route('/tambah')
+        .post(jsonku.tambahmhs);
 }
