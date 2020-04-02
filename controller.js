ad9dbcf0b3e27f3ee8cb26abf3999f894eec8555
Hengky Wijaya <hannw171@outlook.com>
2020-04-02 16:20:58 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./rest');
 var connection = require('./koneksi');
 
 exports.index = function(req,rest){
-    response.ok("Aplikasi rest api berjalan")
+    response.ok("Aplikasi rest api berjalan",rest)
+};
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamhs = function(req,rest){
+    connection.query('SELECT * from mahasiswa', function(error,rows,fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, rest)
+        }
+    });
 };
